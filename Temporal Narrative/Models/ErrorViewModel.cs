22caa42e27adc3e9d3c9d060cd522d3a4e7bd08b
Alex <dtdknown@gmail.com>
2017-10-23 01:34:52 -0400

    v 0.1.10
    First Github commit

---
+++ Temporal Narrative/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Temporal_Narrative.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
